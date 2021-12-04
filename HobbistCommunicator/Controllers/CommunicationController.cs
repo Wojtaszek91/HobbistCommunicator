203af84992f51796dc536c6876a9d8dc32a49c2a
Michał Wojtaszek <michal.wojtaszek@microsoft.wsei.edu.pl>
2021-12-04 17:06:26 +0100

    Add project files.

---
+++ HobbistCommunicator/Controllers/CommunicationController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HobbistCommunicator.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CommunicationController : ControllerBase
+    {
+        private readonly ILogger<CommunicationController> _logger;
+
+        public CommunicationController()
+        {
+        }
+
+        [HttpPost("SendMessage")]
+        public async Task<IActionResult> SendMessage([FromBody] AddHashtagNameDto newHashTagRequest)
+        {
+        }
+    }
+}
